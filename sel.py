99923fd19e8b047143e09defe1415720efa8ca26
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-10-28 16:47:54 +0530

    sel update

--- sel.py
+++ sel.py
@@ -7,6 +7,7 @@ def get_items():
     driver.get("https://www.lazada.sg/gourmet-food-and-gifts/?spm=a2o42.home.cate_6_3.1.191646b5JfWlaR")
     items = driver.find_elements_by_xpath("//div[@data-tracking='product-card']")
     for item in items:
+        print(item.text)
         link = item.find_elements_by_xpath("//a")[0]
         print(link.get_attribute("href"))
     
